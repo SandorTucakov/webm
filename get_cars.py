6fbe547fd926cf1d3ecfece237b79ec356e2fc16
Sandor <sandorcaetano@yahoo.com.br>
2016-07-30 08:54:38 -0300

    changes and gitignore

--- get_cars.py
+++ get_cars.py
@@ -9,8 +9,30 @@ import time
 from lxml import etree
 import urllib2
 from unidecode import unidecode
+from time import gmtime, strftime
 
-def get_cars(links, filename='results'):
+def requestator(url):
+    print url
+    req = Request(url)
+    try:
+        response = urlopen(req)
+    except URLError as e:
+        status = 'error'
+        if hasattr(e, 'reason'):
+            print('We failed to reach a server.')
+            print('Reason: ', e.reason)
+        elif hasattr(e, 'code'):
+            print('The server couldn\'t fulfill the request.')
+            print('Error code: ', e.code)
+    else:
+        status = 'ok'
+        htmlparser = etree.HTMLParser()
+        tree = etree.parse(response, htmlparser)
+
+    return status, tree
+
+
+def get_cars(links, filename='./webm/results'):
     campos = dict(
         estado='//head/meta[@name="wm.dt_estado"]/@content',
         cidade='//head/meta[@name="wm.dt_cidade"]/@content',
@@ -39,26 +61,19 @@ def get_cars(links, filename='results'):
     results = []
     count = 0
     for li in links:
-        req = Request(li)
-        try:
-            response = urlopen(req)
-        except URLError as e:
-            time.sleep(5)  # delays for 5 seconds
-            if hasattr(e, 'reason'):
-                print('We failed to reach a server.')
-                print('Reason: ', e.reason)
-            elif hasattr(e, 'code'):
-                print('The server couldn\'t fulfill the request.')
-                print('Error code: ', e.code)
-        else:
-            htmlparser = etree.HTMLParser()
-            tree = etree.parse(response, htmlparser)
+        status, tree = requestator(li)
+        chaves = []
+        vals = []
+        chaves.append('link')
+        vals.append(li)
 
-            chaves = []
-            vals = []
+        #time
+        chaves.append('download_date')
+        vals.append(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 
-            chaves.append('link')
-            vals.append(li)
+        if status == 'ok':
+            #htmlparser = etree.HTMLParser()
+            #tree = etree.parse(response, htmlparser)
 
             for key, value in campos.iteritems():
                 parsed = None
@@ -66,45 +81,32 @@ def get_cars(links, filename='results'):
                 if parsed:
                     chaves.append(key)
                     vals.append(unidecode(parsed[0]))
+
             ### FIPE
             l1 = tree.xpath('//div[@class="pad-oh_gutter-tb pad-l-1 pad-r-1 bg-gray-light2"]//@href')
-
             if l1:
-                #r1 = urllib2.urlopen(l1[0])
-                req1 = Request(l1[0])
-                try:
-                    r1 = urlopen(req1)
-                except URLError as e:
-                    time.sleep(5)  # delays for 5 seconds
-                    if hasattr(e, 'reason'):
-                        print('We failed to reach a server.')
-                        print('Reason: ', e.reason)
-                    elif hasattr(e, 'code'):
-                        print('The server couldn\'t fulfill the request.')
-                        print('Error code: ', e.code)
+                status, tree1 = requestator(l1[0])
+                chaves.append('FIPE')
+                arv = tree1.xpath('//p[@class="size-21 bold alinha-preco show"]/text()')
+                if arv:
+                    vals.append(arv[0])
                 else:
-                    htmlparser1 = etree.HTMLParser()
-                    tree1 = etree.parse(r1, htmlparser1)
-
-                    chaves.append('FIPE')
-                    vals.append(tree1.xpath('//p[@class="size-21 bold alinha-preco show"]/text()')[0])
+                    vals.append('0')
             else:
                 chaves.append('FIPE')
                 vals.append('0')
 
             results.append(pd.DataFrame(vals, index=chaves).T)
 
-            count += 1
-            if count % 100 == 0 or li==links[-1]:
-                print count
-                final = pd.concat(results, axis=0).reset_index(drop=True)
-                #final.to_csv(filename + '.csv', index=False)
+        count += 1
+        if count % 10 == 0 or li == links[-1]:
+            print count
+            final = pd.concat(results, axis=0).reset_index(drop=True)
+            #final.to_csv(filename + '.csv', index=False)
 
-                with open(filename + '.csv', 'a') as f:
-                    final.to_csv(f, header=False, index=False, sep=';')
-                    f.close()
-                results = []
-
-
+            with open(filename + '.csv', 'a') as f:
+                final.to_csv(f, header=False, index=False, sep=';')
+                f.close()
+            results = []
 
     return final
